598b2253679d4f8b6d31d2ae92f77615f5f85caa
Dremmanuel1 <olugbodi18@gmail.com>
2022-07-07 15:38:11 -0700

    low level programming

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,14 @@
 #include "main.h"
+
 /**
- *  *_isupper - checks if letters are uppercase.
- *   *@c: input.
- *    *
- *     *Return: 1 if c is uppercase
- *      *0 otherwise.
+ * * _isupper - checks if the character is uppercase
+ * * @c: is the int used for the argument of the function.
+ * * Return: 1 if uppercase character 0 if not
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-				{
-							return (1);
-								}
-			else
-					{
-								return (0);
-									}
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
 }
