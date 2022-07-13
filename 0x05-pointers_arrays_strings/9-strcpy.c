e67ad2b3a5dcddb6d4987286a4c3164393c39400
Dremmanuel1 <olugbodi18@gmail.com>
2022-07-12 20:52:45 -0700

    low level programming

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *   * _strcpy - Copy a string
- *     * @dest: Destination value
- *       * @src: Source value
- *         *
- *           * Return: the pointer to dest
- *            /
- *            char *_strcpy(char *dest, char *src)
- *            {
- *            int i;
- *
- *            for (i = 0; src[i] != '\0'; i++)
- *            {
- *            dest[i] = src[i];
- *            }
- *
- *            dest[i++] = '\0';
- *
- *            return (dest);
- *            }
+ * * _strcpy - copies the string pointed to by src into dest
+ * * @dest: destination
+ * * @src: source
+ * * Return: char with copy of string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
